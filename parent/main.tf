204bbb772927ac53d5256b0d01f969c580426a37
omkar <omkar@example.com>
2025-07-08 18:49:11 +0000

    2

---
+++ parent/main.tf
@@ -0,0 +1,6 @@
+module "rg1" {
+    source = "../module/resource_group"
+    azurerm_resource_group =  "rg1"
+    azurerm_location =  "West Europe"
+  
+}
---
+++ parent/module/resource_group/variable.tf
@@ -0,0 +1,11 @@
+variable "azurerm_resource_group" {
+    description = "rgname"
+    type = string
+  
+}
+variable "azurerm_location" {
+    description = "location"
+    type = string
+    
+  
+}
